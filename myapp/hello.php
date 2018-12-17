53bd7adca68693951abaaaf3a54d7789a609ef16
laura.cain@ground-control.co.uk <laura.cain@ground-conrol.co.uk>
2018-12-17 16:30:50 +0000

    TestCommit

--- myapp/hello.php
+++ myapp/hello.php
@@ -1,31 +1,28 @@
 <?php
 
-echo 'TEST'
+require './MyApp/MyApp.class.php';
 
-//require './MyApp/MyApp.class.php';
+require './MyApp/Db.class.php';
 
-//require './MyApp/Db.class.php';
+require './.myapp-config/myapp.config.php';
 
-//require './.myapp-config/myapp.config.php';
+$MyApp = new \MyApp\MyApp (MY_APP_DSN,MY_APP_USR,MY_APP_PWD);
 
-//$MyApp = new \MyApp\MyApp (MY_APP_DSN,MY_APP_USR,MY_APP_PWD);
+$data = $MyApp->dbQuery ('SELECT `user_Display_Name` FROM `myapp_user` WHERE `user_Email`=?','testuser@no.where');
 
-//$data = $MyApp->dbQuery ('SELECT `user_Display_Name` FROM `myapp_user` WHERE `user_Email`=?','testuser@no.where');
-
-//ob_start ();
+ob_start ();
 
 ?>
 
-<!--<h2>
-<?php 
-//echo htmlspecialchars ($data[0]['user_Display_Name']); 
-?>
-</h2>-->
+<h2>Hello <?php echo htmlspecialchars ($data[0]['user_Display_Name']); ?></h2>
 
 <?php
 
-//$MyApp->output = ob_get_clean ();
+$MyApp->output = ob_get_clean ();
 
-//require './MyApp/template.html';
+require './MyApp/template.html';
 
 ?>
+
+
+hello hello
